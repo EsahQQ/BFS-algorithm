e367f9828b9308672206917cb5b079930c687a8b
Kovalev Ivan <kovalevvanc@gmail.com>
2026-02-12 11:24:56 +0300

    Add: usercontroller

---
+++ Assets/_Project/Scripts/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace _Project.Scripts
+{
+    public class UserController : MonoBehaviour
+    {
+        private Camera _cam;
+
+        public event Action<Transform> OnUserClickOnCell;
+        private void Awake()
+        {
+            _cam = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = _cam.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out var hit, 1000f))
+                    if (hit.transform != null)
+                        OnUserClickOnCell?.Invoke(hit.transform);
+            }
+        }
+    }
+}
