50dc658ae38b8ec4eb0f40d6618c3ef743e8f3e9
Kovalev Ivan <kovalevvanc@gmail.com>
2026-02-12 12:31:17 +0300

    Add: cell change color on become/stop being keypoint

---
+++ Assets/_Project/Scripts/GridVisual.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class GridVisual : MonoBehaviour
+    {
+        [SerializeReference] private Material redMaterial;
+        [SerializeReference] private Material greenMaterial;
+        [SerializeReference] private Material blueMaterial;
+        [SerializeReference] private Material defaultMaterial;
+        private GameObject _startCell;
+        private GameObject _endCell;
+        private GridController _gridController;
+
+        private void Awake()
+        {
+            _gridController = GetComponent<GridController>();
+        }
+
+        private void OnEnable()
+        {
+            _gridController.OnCellBecomeKeyPoint += ChangeKeyPointCell;
+        }
+
+        private void OnDisable()
+        {
+            _gridController.OnCellBecomeKeyPoint -= ChangeKeyPointCell;
+        }
+
+        private void ChangeKeyPointCell(GameObject obj, int dir)
+        {
+            if (dir == 0)
+            {
+                if (_startCell != null)
+                    _startCell.GetComponent<Renderer>().material = defaultMaterial;
+                _startCell = obj;
+                _startCell.GetComponent<Renderer>().material = greenMaterial;
+            }
+
+            if (dir == 1)
+            {
+                if (_endCell != null)
+                    _endCell.GetComponent<Renderer>().material = defaultMaterial;
+                _endCell = obj;
+                _endCell.GetComponent<Renderer>().material = redMaterial;
+            }
+        }
+    }
+}
