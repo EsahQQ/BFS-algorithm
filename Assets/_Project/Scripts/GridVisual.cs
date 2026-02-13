aee91299e0f0a042f2d9a354f525ec3d330b45b7
Kovalev Ivan <kovalevvanc@gmail.com>
2026-02-13 15:32:40 +0300

    Add: BFS visualize anim

--- Assets/_Project/Scripts/GridVisual.cs
+++ Assets/_Project/Scripts/GridVisual.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Project.Scripts
 {
     public class GridVisual : MonoBehaviour
     {
-        [SerializeReference] private Material redMaterial;
-        [SerializeReference] private Material greenMaterial;
-        [SerializeReference] private Material blueMaterial;
+        [SerializeReference] private Material endPointMaterial;
+        [SerializeReference] private Material startPointMaterial;
+        [SerializeReference] private Material foundCellMaterial;
+        [SerializeReference] private Material pathMaterial;
         [SerializeReference] private Material defaultMaterial;
         private GameObject _startCell;
         private GameObject _endCell;
@@ -21,11 +24,42 @@ namespace _Project.Scripts
         private void OnEnable()
         {
             _gridController.OnCellBecomeKeyPoint += ChangeKeyPointCell;
+            _gridController.OnCancelFoundAlgorithm += DoFoundVisual;
         }
+
+        private void DoFoundVisual(Dictionary<(int, int), (int, int)> e)
+        {
+            StartCoroutine(ChangeFoundCellsColor(e));
+        }
+
+        private IEnumerator ChangeFoundCellsColor(Dictionary<(int, int), (int, int)> e)
+        {
+            foreach (var c in e.Keys)
+            {
+                if (c != _gridController.GetStartIndex()  && c != _gridController.GetEndIndex())
+                {
+                    _gridController.GetGrid()[c.Item1, c.Item2].GetComponent<Renderer>().material = foundCellMaterial;
+                    yield return new WaitForSeconds(0.005f);
+                }
+            }
+            
+            DoPathVisual(e, e[_gridController.GetEndIndex()]);
+        }
+        
+        private void DoPathVisual(Dictionary<(int, int), (int, int)> e, (int, int) ind)
+        {
+            if (ind == _gridController.GetStartIndex())
+                return;
+            
+            _gridController.GetGrid()[ind.Item1, ind.Item2].GetComponent<Renderer>().material = pathMaterial;
+            DoPathVisual(e, e[ind]);
+        }
+        
 
         private void OnDisable()
         {
             _gridController.OnCellBecomeKeyPoint -= ChangeKeyPointCell;
+            _gridController.OnCancelFoundAlgorithm += DoFoundVisual;
         }
 
         private void ChangeKeyPointCell(GameObject obj, int dir)
@@ -35,7 +69,7 @@ namespace _Project.Scripts
                 if (_startCell != null)
                     _startCell.GetComponent<Renderer>().material = defaultMaterial;
                 _startCell = obj;
-                _startCell.GetComponent<Renderer>().material = greenMaterial;
+                _startCell.GetComponent<Renderer>().material = startPointMaterial;
             }
 
             if (dir == 1)
@@ -43,7 +77,7 @@ namespace _Project.Scripts
                 if (_endCell != null)
                     _endCell.GetComponent<Renderer>().material = defaultMaterial;
                 _endCell = obj;
-                _endCell.GetComponent<Renderer>().material = redMaterial;
+                _endCell.GetComponent<Renderer>().material = endPointMaterial;
             }
         }
     }
