81694e0ab395f72a678dd6782a9c3729a4f35cff
Kovalev Ivan <kovalevvanc@gmail.com>
2026-02-13 15:32:13 +0300

    Add: BFS Algorithm

--- Assets/_Project/Scripts/GridController.cs
+++ Assets/_Project/Scripts/GridController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Project.Scripts
@@ -8,15 +9,87 @@ namespace _Project.Scripts
         [SerializeField] private GameObject cellPrefab;
         [SerializeField] private int size = 10;
         [SerializeField] private UserController userController;
-        private (int, int) _startInd;
-        private (int, int) _endInd;
+        private (int, int) _startInd = (-1, -1);
+        private (int, int) _endInd = (-1, -1);
         private GameObject[,] _grid;
 
         public event Action<GameObject, int> OnCellBecomeKeyPoint;
+        public event Action<Dictionary<(int, int), (int, int)>> OnCancelFoundAlgorithm;
 
         private void OnEnable()
         {
             userController.OnUserClickOnCell += ChangeKeyPoint;
+            userController.OnUserClickOnStartButton += TryFoundPath;
+        }
+        
+        private void Start()
+        {
+            _grid = new GameObject[size, size];
+            SpawnGrid();
+        }
+
+        private void OnDisable()
+        {
+            userController.OnUserClickOnCell -= ChangeKeyPoint;
+            userController.OnUserClickOnStartButton -= TryFoundPath;
+        }
+
+        private void TryFoundPath()
+        {
+            if (_grid == null || _startInd == (-1, -1) || _endInd == (-1, -1))
+                return;
+
+            var q = new Queue<(int, int)>();
+            var cameFrom = new Dictionary<(int, int), (int, int)>()
+            {
+                {_startInd, (-1, -1)}
+            };
+            q.Enqueue(_startInd);
+            while (q.Count > 0)
+            {
+                var (r, c) = q.Dequeue();
+                (int, int)[] directions = { (r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1) };
+
+                foreach (var next in directions)
+                {
+                    int nr = next.Item1;
+                    int nc = next.Item2;
+
+                    if (nr >= 0 && nr < _grid.GetLength(0) && nc >= 0 && nc < _grid.GetLength(1))
+                    {
+                        if (!cameFrom.ContainsKey(next))
+                        {
+                            cameFrom.Add(next, (r, c));
+
+                            if (next == _endInd)
+                            {
+                                OnCancelFoundAlgorithm?.Invoke(cameFrom);
+                                return;
+                            }
+                                
+
+                            q.Enqueue(next);
+                        }
+                    }
+                }
+            }
+            
+            OnCancelFoundAlgorithm?.Invoke(cameFrom);
+        }
+
+        public GameObject [,] GetGrid()
+        {
+            return _grid;
+        }
+        
+        public (int, int) GetEndIndex()
+        {
+            return _endInd;
+        }
+        
+        public (int, int) GetStartIndex()
+        {
+            return _startInd;
         }
 
         private void ChangeKeyPoint(Transform obj, int dir)
@@ -44,17 +117,6 @@ namespace _Project.Scripts
                         return (i,j);
 
             return (-1, -1);
-        }
-        
-        private void Start()
-        {
-            _grid = new GameObject[size, size];
-            SpawnGrid();
-        }
-
-        private void OnDisable()
-        {
-            userController.OnUserClickOnCell -= ChangeKeyPoint;
         }
 
         private void SpawnGrid()
