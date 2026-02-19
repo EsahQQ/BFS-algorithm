ed4c2e5d42ca597b350a8e2e0b2b2273caa3a04a
Kovalev Ivan <kovalevvanc@gmail.com>
2026-02-19 08:37:16 +0300

    Add: change cell wall handler

--- Assets/_Project/Scripts/GridController.cs
+++ Assets/_Project/Scripts/GridController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace _Project.Scripts
@@ -15,13 +16,22 @@ namespace _Project.Scripts
 
         public event Action<GameObject, int> OnCellBecomeKeyPoint;
         public event Action<Dictionary<(int, int), (int, int)>> OnCancelFoundAlgorithm;
+        public event Action<GameObject, bool> OnChangeCellWall;
 
         private void OnEnable()
         {
             userController.OnUserClickOnCell += ChangeKeyPoint;
             userController.OnUserClickOnStartButton += TryFoundPath;
+            userController.OnUserDoWall += ChangeCellWall;
         }
-        
+
+        private void ChangeCellWall(Transform obj)
+        {
+            var cell = obj.GetComponent<Cell>();
+            cell.IsWall = !cell.IsWall;
+            OnChangeCellWall?.Invoke(obj.gameObject, cell.IsWall);
+        }
+
         private void Start()
         {
             _grid = new GameObject[size, size];
@@ -32,6 +42,7 @@ namespace _Project.Scripts
         {
             userController.OnUserClickOnCell -= ChangeKeyPoint;
             userController.OnUserClickOnStartButton -= TryFoundPath;
+            userController.OnUserDoWall -= ChangeCellWall;
         }
 
         private void TryFoundPath()
@@ -54,9 +65,12 @@ namespace _Project.Scripts
                 {
                     int nr = next.Item1;
                     int nc = next.Item2;
-
+                    
                     if (nr >= 0 && nr < _grid.GetLength(0) && nc >= 0 && nc < _grid.GetLength(1))
                     {
+                        if (_grid[nr, nc].gameObject.GetComponent<Cell>().IsWall)
+                            continue;
+                         
                         if (!cameFrom.ContainsKey(next))
                         {
                             cameFrom.Add(next, (r, c));
@@ -66,8 +80,7 @@ namespace _Project.Scripts
                                 OnCancelFoundAlgorithm?.Invoke(cameFrom);
                                 return;
                             }
-                                
-
+                            
                             q.Enqueue(next);
                         }
                     }
@@ -94,36 +107,33 @@ namespace _Project.Scripts
 
         private void ChangeKeyPoint(Transform obj, int dir)
         {
-            var ind = FindInGrid(obj.gameObject);
+            var cell = obj.GetComponent<Cell>();
+            var ind = cell.Index;
             if (ind == (-1, -1)) return;
             switch (dir)
             {
                 case 0:
                     OnCellBecomeKeyPoint?.Invoke(obj.gameObject, dir);
+                    cell.IsWall = false;
                     _startInd = ind;
                     break;
                 case 1:
                     OnCellBecomeKeyPoint?.Invoke(obj.gameObject, dir);
+                    cell.IsWall = false;
                     _endInd = ind;
                     break;
             }
-        }
-
-        private (int, int) FindInGrid(GameObject obj)
-        {
-            for (var i = 0; i < _grid.GetLength(0); i++)
-                for (var j = 0; j < _grid.GetLength(1); j++)
-                    if (obj == _grid[i, j])
-                        return (i,j);
-
-            return (-1, -1);
         }
 
         private void SpawnGrid()
         {
             for (int i = 0; i < size; i++)
                 for (int j = 0; j < size; j++)
-                    _grid[i, j] = Instantiate(cellPrefab, new Vector3(i, 0, j), Quaternion.identity);
+                {
+                    var cell = Instantiate(cellPrefab, new Vector3(i, 0, j), Quaternion.identity);
+                    cell.GetComponent<Cell>().Index = (i, j);
+                    _grid[i, j] = cell;
+                }
             
             Camera.main!.transform.position = new Vector3((size - 1) / 2f, size * 0.6f, (size - 1) / 2f);
         }
